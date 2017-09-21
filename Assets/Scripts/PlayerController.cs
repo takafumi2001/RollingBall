04d62196c3d10b414a23b79aa769cae63ada683d
takafumi2001 <2001takafumi@gmail.com>
2017-09-21 16:18:15 +0900

    First Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	private void FixedUpdate ()
+	{
+		float x = Input.GetAxis ("Horizontal");
+		float z = Input.GetAxis ("Vertical");
+
+		Rigidbody rigidbody = GetComponent<Rigidbody> ();
+
+		rigidbody.AddForce (x * 5, 0, z * 5);
+	}
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour 
+{
+	public Text GameClearLabel;
+	public Text GameOverLabel;
+	public Button GameOverButton;
+
+	private void Start () 
+	{
+		// 開始時に表示を消す
+		GameClearLabel.gameObject.SetActive(false);	
+		GameOverLabel.gameObject.SetActive (false);
+		GameOverButton.gameObject.SetActive (false);
+	}
+
+	public void ShowGameClearLabel()
+	{
+		// 開始時に表示する
+		GameClearLabel.gameObject.SetActive(true);
+		GameOverButton.gameObject.SetActive (true);
+	}
+
+	public void ShowGameOverLabel()
+	{
+		GameOverLabel.gameObject.SetActive (true);
+		GameOverButton.gameObject.SetActive (true);
+	}
+}
